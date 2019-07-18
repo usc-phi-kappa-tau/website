66710789d17598312f5ac176c47921172127636e
Ralph Sun <ralphsun@usc.edu>
2019-07-17 20:29:14 -0400

    now this better work

--- src/components/Fraternity.jsx
+++ src/components/Fraternity.jsx
@@ -14,10 +14,10 @@ export default (props) => (
 
         <p>The mission of Phi Kappa Tau is to champion a lifelong commitment to brotherhood, learning, ethical leadership and exemplary character.</p>
       </div>
-      <br />
-      <div className="Inclusive">
-        <p>Please visit the <a href="www.phikappatau.org">Phi Kappa Tau National Fraternity</a> website to learn more.</p>
-      </div>
+      
+    </div>
+    <div className="Inclusive">
+      <p>Please visit the <a href="www.phikappatau.org">Phi Kappa Tau National Fraternity</a> website to learn more.</p>
     </div>
   </div>
 )
