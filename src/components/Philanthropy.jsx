deb230ea57ca6f4404411e7d9f706e3cccd64838
Ralph Sun <ralphsun@usc.edu>
2019-07-17 23:17:02 -0400

    added links to philantrophy sites

--- src/components/Philanthropy.jsx
+++ src/components/Philanthropy.jsx
@@ -5,8 +5,8 @@ const Philanthropy = () => {
         <div className="Inclusive">
             <div>
                 <p>
-                    Phi Kappa Tau is proud to partner with SeriousFun Children's Network, 
-                    an international organization that allows children with serious or terminal illnesses 
+                    Phi Kappa Tau is proud to partner with <a href="https://seriousfunnetwork.org/">
+                    SeriousFun Children's Network</a>, an international organization that allows children with serious or terminal illnesses 
                     to attend camps and have fun. Founded by actor and Phi Tau alumnus Paul Newman in 1988, 
                     SeriousFun camps have served over 1,000,000 children in 20 different countries - 
                     all free of charge.  Through this partnership, Phi Tau has helped raise over 
@@ -16,8 +16,8 @@ const Philanthropy = () => {
                 <p>
                     USC Phi Kappa Tau also is proud to give back to the local community.  
                     We have helped to raise money and volunteer for many local organizations, 
-                    including Swim with Mike, an organization dedicated to provide scholarships 
-                    for physically challenged athletes.                    
+                    including <a href="https://www.swimwithmike.org/">Swim with Mike</a>, 
+                    an organization dedicated to provide scholarships for physically challenged athletes.                    
                 </p>
             </div>
         </div >
