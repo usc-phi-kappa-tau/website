ef5ecefd4e9bd626a8db0083a87a74ef7f19a14b
Ralph Sun <ralphsun@usc.edu>
2019-08-29 13:03:36 -0700

    updated events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -5,18 +5,10 @@ import EventCard from './EventCard';
 const Events = () => {
     const events = [
         {
-            title: "PKT Off-campus Day 2",
-            date: "6:00PM August 28, 2019",
-            description: "Casino Night",
-            location: "2715 Portland Street",
-            calendarStartTime: '2019-08-28T18:00:00-07:00',
-            calendarEndTime: '2019-08-28T21:00:00-07:00'
-        },
-        {
             title: "PKT Blue Chip Dinner",
             date: "7:30PM August 29, 2019",
-            description: "Blue Chip @ El Cholo",
-            location: "1037 South Flower Street",
+            description: "Blue Chip @ PKT House",
+            location: "2715 Portland Street",
             calendarStartTime: '2019-08-28T19:30:00-07:00',
             calendarEndTime: '2019-08-28T21:30:00-07:00'
         },
@@ -31,6 +23,22 @@ const Events = () => {
         {
             title: "USC vs. Fresno State",
             date: "7:30PM August 31, 2019",
+            description: "Gameday and Tailgate",
+            location: "LA Memorial Coliseum",
+            calendarStartTime: '2019-08-28T19:30:00-07:00',
+            calendarEndTime: '2019-08-28T21:30:00-07:00'
+        },
+        {
+            title: "Meeting",
+            date: "6:30PM September 1, 2019",
+            description: "Phi Tau Chapter Meeting",
+            location: "2715 Portland Street",
+            calendarStartTime: '2019-08-28T19:30:00-07:00',
+            calendarEndTime: '2019-08-28T21:30:00-07:00'
+        },
+        {
+            title: "USC vs. Stanford",
+            date: "7:30PM September 7, 2019",
             description: "Gameday and Tailgate",
             location: "LA Memorial Coliseum",
             calendarStartTime: '2019-08-28T19:30:00-07:00',
