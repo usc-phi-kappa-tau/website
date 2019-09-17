187f7aeef652f770df0b71b699a5ac39d6391a87
Ralph Sun <ralphsun@usc.edu>
2019-09-17 12:02:55 -0700

    fixing calendar stuff

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -5,6 +5,38 @@ import EventCard from './EventCard';
 const Events = () => {
     const events = [
         {
+            title: "Mixer",
+            date: "TBD",
+            description: "Mixer w/ Theta",
+            location: "TBD",
+            calendarStartTime: '2019-09-18T20:30:00-07:00',
+            calendarEndTime: '2019-09-18T21:30:00-07:00'
+        },
+        {
+            title: "Tailgate",
+            date: "TBD September 20, 2019",
+            description: "Gameday Tailgate",
+            location: "2715 Portland Street",
+            calendarStartTime: '2019-08-28T19:30:00-07:00',
+            calendarEndTime: '2019-08-28T21:30:00-07:00'
+        },
+        {
+            title: "Gameday",
+            date: "6:00-9:30PM September 20, 2019",
+            description: "USC vs. Utah",
+            location: "LA Memorial Coliseum",
+            calendarStartTime: '2019-09-20T19:30:00-07:00',
+            calendarEndTime: '2019-09-20T21:30:00-07:00'
+        },
+        {
+            title: "ADPi Row Cup",
+            date: "TBD September 21, 2019",
+            description: "ADPi Philanthropy",
+            location: "TBD",
+            calendarStartTime: '2019-08-28T19:30:00-07:00',
+            calendarEndTime: '2019-08-28T21:30:00-07:00'
+        },
+        {
             title: "Deliveries",
             date: "5:00-6:00PM September 16, 2019",
             description: "Sorority Deliveries",
@@ -25,38 +57,6 @@ const Events = () => {
             date: "7:00-8:00PM September 16, 2019",
             description: "PKT Chapter Meeting",
             location: "2715 Portland Street",
-            calendarStartTime: '2019-08-28T19:30:00-07:00',
-            calendarEndTime: '2019-08-28T21:30:00-07:00'
-        },
-        {
-            title: "Mixer",
-            date: "TBD",
-            description: "Mixer w/ Theta",
-            location: "TBD",
-            calendarStartTime: '2019-08-28T19:30:00-07:00',
-            calendarEndTime: '2019-08-28T21:30:00-07:00'
-        },
-        {
-            title: "Tailgate",
-            date: "TBD September 20, 2019",
-            description: "Gameday Tailgate",
-            location: "2715 Portland Street",
-            calendarStartTime: '2019-08-28T19:30:00-07:00',
-            calendarEndTime: '2019-08-28T21:30:00-07:00'
-        },
-        {
-            title: "Gameday",
-            date: "6:00-9:30PM September 20, 2019",
-            description: "USC vs. Utah",
-            location: "LA Memorial Coliseum",
-            calendarStartTime: '2019-08-28T19:30:00-07:00',
-            calendarEndTime: '2019-08-28T21:30:00-07:00'
-        },
-        {
-            title: "ADPi Row Cup",
-            date: "TBD September 21, 2019",
-            description: "ADPi Philanthropy",
-            location: "TBD",
             calendarStartTime: '2019-08-28T19:30:00-07:00',
             calendarEndTime: '2019-08-28T21:30:00-07:00'
         }
