0d5a3b921d0e716b5fdb742d70cda167a487a8b9
Ralph Sun <ralphsun@usc.edu>
2019-12-24 19:54:05 -0500

    updated events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -5,44 +5,28 @@ import EventCard from './EventCard';
 const Events = () => {
     const events = [
         {
-            title: "Mixer",
-            date: "TBD",
-            description: "Mixer w/ Alpha Phi",
-            location: "TBD",
+            title: "Rush Kickoff",
+            date: "5:30-7:30PM January 9, 2020",
+            description: "IFC Rush Kickoff",
+            location: "TCC Ballroom",
             calendarStartTime: '2019-09-25T18:30:00-07:00',
             calendarEndTime: '2019-09-25T19:30:00-07:00'
         },
         {
-            title: "Alumni Gameday BBQ",
-            date: "12:30-4:00PM September 28, 2019",
-            description: "Alumni BBQ/Fundraiser",
+            title: "House Tours",
+            date: "1:00-7:00PM January 10, 2020",
+            description: "House Tours Round 1",
             location: "2715 Portland Street",
-            calendarStartTime: '2019-09-28T12:30:00-07:00',
-            calendarEndTime: '2019-09-28T16:00:00-07:00'
+            calendarStartTime: '2019-09-25T18:30:00-07:00',
+            calendarEndTime: '2019-09-25T19:30:00-07:00'
         },
         {
-            title: "Deliveries",
-            date: "5:00-6:00PM September 30, 2019",
-            description: "Sorority Deliveries",
-            location: "The Row",
-            calendarStartTime: '2019-09-30T17:00:00-07:00',
-            calendarEndTime: '2019-09-30T18:00:00-07:00'
-        },
-        {
-            title: "Eboard Meeting",
-            date: "6:30-7:00PM September 30, 2019",
-            description: "PKT Eboard Meeting",
+            title: "House Tours",
+            date: "1:00-7:00PM January 11, 2020",
+            description: "House Tours Round 1",
             location: "2715 Portland Street",
-            calendarStartTime: '2019-09-30T18:30:00-07:00',
-            calendarEndTime: '2019-09-30T19:00:00-07:00'
-        },
-        {
-            title: "Meeting",
-            date: "7:00-8:00PM September 30, 2019",
-            description: "PKT Chapter Meeting",
-            location: "2715 Portland Street",
-            calendarStartTime: '2019-09-30T19:00:00-07:00',
-            calendarEndTime: '2019-09-30T20:00:00-07:00'
+            calendarStartTime: '2019-09-25T18:30:00-07:00',
+            calendarEndTime: '2019-09-25T19:30:00-07:00'
         }
     ];
     return (
